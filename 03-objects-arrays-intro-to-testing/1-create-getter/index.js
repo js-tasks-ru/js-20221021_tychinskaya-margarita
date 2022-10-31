2ca6b3b230a043a6b1591d7ea3ca53dbb565d30d
Margarita Tychinskaya <haruhii.t@gmail.com>
2022-10-31 15:02:53 +0300

    3rd task 1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const arrPath = path.split(".");
+    const getter = (obj) => {
+        let findObj = obj;
+        for (var i = 0; i < arrPath.length; i++) {
+            findObj = findObj[arrPath[i]];
+            if (!findObj)
+              break;
+          }
+          return findObj;
+    }
+    return getter;
 }
